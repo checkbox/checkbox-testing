b1834855166641c983d30bc2304af33ce18a34ae
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-11-05 15:56:25 +0100

    Add environment for headless plainbox testings

---
+++ plainbox/README.md
@@ -0,0 +1,17 @@
+About
+=====
+
+This vagrant environment sets up vanilla Ubuntu 12.04 and adds the required set
+of PPAs to install plainbox. The plainbox package is a development tool and
+swiss-army-knife for the new core of checkbox, known as plainbox
+
+Prerequisites
+=============
+
+You will need a working Vagrant installation.
+
+Usage
+=====
+
+Run 'vagrant up' and wait for the virtual machine to provision. Then run
+'vagrant ssh' and you should be able to use 'plainbox'
---
+++ plainbox/manifests/default.pp
@@ -0,0 +1,47 @@
+class base {
+  $apt_proxy = ""
+  if $apt_proxy {
+    file { 'apt-get proxy config':
+      before  => Exec['apt-get update', 'apt-get dist-upgrade'],
+      path    => '/etc/apt/apt.conf.d/00proxy',
+      ensure  => present,
+      content => "Acquire::http::Proxy \"${apt_proxy}\";\n"
+    }
+  }
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
+  }
+  exec { 'apt-get dist-upgrade':
+    require => Exec['apt-get update'],
+    command => '/usr/bin/apt-get dist-upgrade --yes'
+  }
+}
+
+class plainbox {
+  package {
+    "python-software-properties":   ensure => installed
+  }
+  exec { 'Enable PPA checkbox-dev/ppa':
+    require => Package['python-software-properties'],
+    before  => Exec['apt-get update', 'apt-get dist-upgrade'],
+    command => "/usr/bin/add-apt-repository --yes ppa:checkbox-dev/ppa",
+    onlyif  => "/usr/bin/test ! -e /etc/apt/sources.list.d/checkbox-dev-ppa-precise.list",
+  }
+  package { "plainbox-insecure-policy":
+    require => [
+      Exec['Enable PPA checkbox-dev/ppa'],
+      Exec['apt-get update']
+    ],
+    ensure => installed
+  }
+  package { "plainbox":
+    require => [
+      Exec['Enable PPA checkbox-dev/ppa'],
+      Exec['apt-get update']
+    ],
+    ensure => installed
+  }
+}
+
+include base
+include plainbox 
